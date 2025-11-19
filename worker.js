86e9b7fd79cf8fc656883fd3079eb70303e4f597
axcellsz <122135545+axcellsz@users.noreply.github.com>
2025-11-19 23:22:48 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,18 @@
 addEventListener("fetch", event => {
-  event.respondWith(handle(event.request))
-})
+  event.respondWith(handle(event.request, event));
+});
 
-async function handle(req) {
-  return new Response("Hello from Cloudflare Worker!", {
+async function handle(req, event) {
+  const env = event.target.env; // Cloudflare mengisi env ke sini secara internal
+
+  // Tulis ke KV
+  await env.MY_KV.put("hello", "world");
+
+  // Baca dari KV
+  const value = await env.MY_KV.get("hello");
+
+  return new Response("KV result: " + value, {
     status: 200,
     headers: { "Content-Type": "text/plain; charset=utf-8" }
-  })
+  });
 }
